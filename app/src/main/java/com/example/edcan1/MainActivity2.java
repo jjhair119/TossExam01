aeeef4c9e6c761763cee95a806e7cee997aa8dc4
지민경 <63912449+jjhair119@users.noreply.github.com>
2020-07-10 21:07:14 +0900

    0710

---
+++ app/src/main/java/com/example/edcan1/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.edcan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.edcan1.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    ActivityMain2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main2);
+
+        binding.btnMain2.setOnClickListener(view -> binding.txtMain2.setText(binding.edtMain2.getText()));
+    }
+}
